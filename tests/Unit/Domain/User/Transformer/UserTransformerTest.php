607f51627c07b7958b16f092c8eea8db0259a6cf
Maxime Beaudoin <firalabs@gmail.com>
2020-03-27 13:22:18 +0000

    Add unit test

---
+++ tests/Unit/Domain/User/Transformer/UserTransformerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit\Domain\User\Transformer;
+
+use App\Domain\User\Entity\User;
+use App\Domain\User\Transformer\UserTransformer;
+use Tests\TestCase;
+
+class UserTransformerTest extends TestCase
+{
+    public function testDataWasConvertedProperly()
+    {
+        // Data we want to test
+        $user = new User([
+            'name' => 'John',
+            'email' => 'john.doe@hotmail.com'
+        ]);
+        $user->id = 1;
+
+        // Transformer we want to test
+        $transformer = new UserTransformer();
+
+        // What we expect
+        $this->assertSame([
+            'id' => 1,
+            'name' => 'John',
+            'email' => 'john.doe@hotmail.com'
+        ], $transformer->transform($user));
+
+    }
+}
