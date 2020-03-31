ffbe978f0f85eaff1a4788208c316904b5b2a992
Maxime Beaudoin <firalabs@gmail.com>
2020-03-31 20:00:45 +0000

    Disable sanctum middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,7 +29,6 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
